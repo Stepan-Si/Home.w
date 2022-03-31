6180fb08c3df4ec3bfdf50e5fa9b7c16218bf4e7
Stepan.Si <101666682+Stepan-Si@users.noreply.github.com>
2022-03-31 22:30:54 +0300

    Functions 1.2

---
+++ Project1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void SetCursorPosition(int x, int y, int c);
+
+int main()
+{
+	SetCursorPosition(7, 5, 12);
+    system("pause >> NUL");
+}
+
+
+void SetCursorPosition(int x, int y, int c)
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD position;
+	position.X = x;
+	position.Y = y;
+	SetConsoleCursorPosition(h, position);
+	SetConsoleTextAttribute(h, c);
+	cout << char(3) << "\n";
+}
